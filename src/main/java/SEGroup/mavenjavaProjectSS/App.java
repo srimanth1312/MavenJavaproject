b7f64cc5add325b87d7f63c47c52608aacf2c933
srimanth1312 <srimanthraokatikaneni@gmail.com>
2022-12-03 11:18:08 +0530

    java created

---
+++ src/main/java/SEGroup/mavenjavaProjectSS/App.java
@@ -0,0 +1,13 @@
+package SEGroup.mavenjavaProjectSS;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello from srimanth!" );
+    }
+}
